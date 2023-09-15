2eff29cf3c45653a2dbcc19e4cba223b3d1d9d8b
busratop <busra.top.m07@gmail.com>
2023-09-15 14:35:02 -0700

    Remove Duplicates from Sorted Array II

---
+++ LeetCode-Cpp/RemoveDuplicatesfromSortedArrayII.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int k = 0;
+        for (auto i : nums) {
+            if (k<2 || i> nums[k - 2]) {
+                nums[k++] = i;
+            }
+        }
+        return k;
+    }
+};
