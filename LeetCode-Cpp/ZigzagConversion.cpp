048e9ac58a9573d3db85a74095b8a00ed5195bf3
Büşra TOP <72290437+busratop@users.noreply.github.com>
2024-09-04 03:19:59 -0700

    Create ZigzagConversion.cpp
    Added Zigzag Conversion Cpp code

---
+++ LeetCode-Cpp/ZigzagConversion.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if (numRows == 1 || numRows >= s.length()) {
+            return s;
+        }
+
+        vector<string> rows(numRows, "");
+        int index = 0, step = 1;
+
+        for (char c : s) {
+            rows[index] += c;
+            if (index == 0) {
+                step = 1;
+            } else if (index == numRows - 1) {
+                step = -1;
+            }
+            index += step;
+        }
+
+        string result;
+        for (const string& row : rows) {
+            result += row;
+        }
+
+        return result;
+    }
+};
