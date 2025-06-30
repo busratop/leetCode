06753ca3c5dcf8af6a5467fdb056d019e7ede0f4
Büşra TOP <72290437+busratop@users.noreply.github.com>
2025-06-30 09:46:37 +0300

    Create CloneGraph.py
    Added Clone Graph python code

---
+++ LeetCode-Python/CloneGraph.py
@@ -0,0 +1,34 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+from typing import Optional
+class Node:
+    def __init__(self, val=0, neighbors=None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        if not node:
+            return None
+
+        visited = {}
+
+        def dfs(n):
+            if n in visited:
+                return visited[n]
+
+            clone = Node(n.val)
+            visited[n] = clone
+
+            for neighbor in n.neighbors:
+                clone.neighbors.append(dfs(neighbor))
+
+            return clone
+
+        return dfs(node)
