71045cc23d2302824de6e7467717d9253cf42afb
Büşra TOP <72290437+busratop@users.noreply.github.com>
2025-10-06 09:45:57 +0300

    Create FactorialTrailingZeroes.py
    Added Factorial Trailing Zeroes

---
+++ LeetCode-Python/FactorialTrailingZeroes.py
@@ -0,0 +1,12 @@
+class Solution:
+    def trailingZeroes(self, n: int) -> int:
+        count = 0
+        # Sayılar 5'in katı olduğunda bir tane 5 çarpanı ekleriz.
+        # Sayılar 25'in katı olduğunda iki tane 5 çarpanı ekleriz (çünkü 25 = 5 * 5).
+        # Sayılar 125'in katı olduğunda üç tane 5 çarpanı ekleriz (çünkü 125 = 5 * 5 * 5).
+        # Ve bu böyle devam eder.
+        # Bu yüzden, n'i 5'e, 25'e, 125'e vb. bölerek her birinden gelen 5 çarpanlarını toplarız.
+        while n >= 5:
+            n //= 5  # Python'da tam sayı bölme için '//' kullanılır.
+            count += n
+        return count
